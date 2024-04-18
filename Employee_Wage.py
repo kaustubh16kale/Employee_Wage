306ce929421bb7d8ed10e9db7ce3b74eb2938b31
Kaustubh Kale <kaustubh.mum.dbda@gmail.com>
2024-04-18 15:46:45 +0530

    [kaustubh Kale] Refactor : Attendance Check

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -1,17 +1,6 @@
 import random as rm
-def Attendance_check():
-    total_attendance=0
-    total_absent=0
-    attendance={}
-    for i in range(1,31):
-        attendance[i]=rm.randint(0,1)
-    for j in attendance.values():
-        if j==0:
-            total_attendance+=1
-        else:
-            total_absent+=1
-    print("total leaves in a month is : ",total_absent)
-    print("Total present in a month is : ",total_attendance)
-    
-
-Attendance_check()
+attendance=rm.randint(0,1)
+if attendance==1:
+    print("employee is present")
+else:
+    print("employee is absent")
